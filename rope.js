d151707879416a06bf2996469fd3181c792aa725
ds326343 <72473335+ds326343@users.noreply.github.com>
2020-12-13 13:51:30 -0800

    Add files via upload

---
+++ rope.js
@@ -0,0 +1,20 @@
+class Rope{
+    constructor(bodyA,pointB){
+        var options = {
+            bodyA:bodyA,
+            pointB:pointB,
+            stiffness:1.0,
+            length:400
+        }
+        this.pointB = pointB;
+        this.rope = Constraint.create(options);
+        World.add(world,this.rope)
+    }
+    display(){
+        var pointA = this.rope.bodyA.position
+        var pointB = this.pointB;
+        strokeWeight(4);
+        stroke(255);
+        line(pointB.x,pointB.y,pointA.x,pointA.y);
+    }
+}
